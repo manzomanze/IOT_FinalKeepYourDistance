17d02c618bc1021a0e5588a1e8ea02225e27c942
manzomanze <manzella.davide97@gmail.com>
2020-07-23 14:43:26 +0200

    correct errorsIDsNode

--- Final.h
+++ Final.h
@@ -4,10 +4,10 @@
 typedef nx_struct my_msg {
   nx_uint16_t id;
 } my_msg_t;
-typedef nx_struct IDsNode {
+typedef nx_struct IDsNode_t {
 	nx_uint16_t id;
-	IDsNode* next;
-}IDsNode_t;
+	IDsNode_t* next;
+} struct IDsNode_t;
 
 enum {
   AM_MY_MSG = 6,
